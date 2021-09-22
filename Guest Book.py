6efae17788aa105810fed06c7f280cfa84c5a7fd
Mhmmdtegar17 <78156473+Mhmmdtegar17@users.noreply.github.com>
2021-09-22 23:29:27 +0700

    Add files via upload

---
+++ Guest Book.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+
+master = tk.Tk()
+master.title("Buku Tamu Perpustakaan")
+master.geometry("300x300")
+
+inputNo = tk.Entry(master)
+inputNama = tk.Entry(master)
+inputJenisKelamin = tk.Entry(master)
+inputPekerjaan = tk.Entry(master)
+inputNomorTelepon = tk.Entry(master)
+inputAlamat = tk.Entry(master)
